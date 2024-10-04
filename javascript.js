40a26be8f20f5d4c10960c3fa8bf8b69ec178441
Johnny Liu <33532804+JohnnyL302@users.noreply.github.com>
2024-10-04 17:08:09 -0400

    Finished playRound function with all choices

--- javascript.js
+++ javascript.js
@@ -11,14 +11,34 @@ function getComputerChoice(num) {
     }
 }
 
-cmp_choice = getComputerChoice(3) 
-console.log(cmp_choice)
-
 function getHumanChoice(num) {
     user_choice = prompt("Please select 'Rock', 'Paper', 'Scissors' ; ")
     return user_choice.toLowerCase(); 
 } 
 
+function playRound(humanChoice, computerChoice) {
+    if ( (humanChoice === "paper" && computerChoice === "scissors") || (humanChoice === "rock" && computerChoice === "paper") 
+        || (humanChoice === "scissors" && computerChoice ===  "rock") ) {
+            console.log(`You lose! ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)} beats ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)}.`)
+            computerScore++; 
+        } 
+    else if ( (humanChoice === "paper" && computerChoice === "rock") || (humanChoice  === "rock" &&  computerChoice === "scissors") || 
+        (humanChoice  === "scissors" && computerChoice ===  "paper") ) {
+            console.log(`You win! ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)} beats ${computerChoice.charAt(0) + computerChoice.slice(1)}.`)
+            humanScore++;
+        }
+    else {
+        console.log("It's a tie!")
+    } 
+
+}
+
+cmp_choice = getComputerChoice(3) 
+console.log(cmp_choice)
 player_choice = getHumanChoice()
 console.log(player_choice)
 
+let humanScore = 0  
+let computerScore = 0
+
+playRound(player_choice, cmp_choice);
